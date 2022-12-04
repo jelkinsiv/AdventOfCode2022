4fca4f3bf9704459bee8d22adca94439cbedd3bf
James Elkins <james.elkins@brightlysoftware.com>
2022-12-04 09:13:54 -0500

    AoC Day 4

---
+++ Day 4/4A.py
@@ -0,0 +1,16 @@
+data_file = open("Day 4/4_data.txt", "r")
+contained_pairs = 0
+
+def doesContain(array1, array2):
+    if (array1[0] <= array2[0]) and (array1[1] >= array2[1]):
+        return True
+    return False
+
+for line in data_file:
+    first_elf, second_elf = [list(map(int,word.strip().split("-"))) for word in line.split(",", 1)]
+
+    print(f"{first_elf} - {second_elf} | ", end= "")
+    if doesContain(first_elf, second_elf) or doesContain(second_elf, first_elf):
+        contained_pairs += 1
+
+print(contained_pairs)
